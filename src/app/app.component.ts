cbc7b093e2c872c0fca0a382b3a3cc56bb11937a
Gak1o <gakiotoni@gmail.com>
2024-09-19 09:23:13 +0300

    Bruuuuuuuuuuuuuv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, HeaderComponent, HomeComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'nyak0';
+  title = 'ecommerceangular';
 }
